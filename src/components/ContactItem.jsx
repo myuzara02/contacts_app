b92527a91bba7b52aff7da294b2835e927286014
myuzara12 <myuzara12@gmail.com>
2023-09-28 11:36:53 +0700

    add input form

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import ContactItemImage from './ContactItemImage';
 import ContactItemBody from './ContactItemBody';
+import DeleteButton from './DeleteButton';
 
-const ContactItem = ({ imageUrl, name, tag }) => {
+const ContactItem = ({ imageUrl, name, tag, id, onDelete }) => {
   return (
     <div className='contact-item'>
-        <ContactItemImage imageUrl={imageUrl}/>
-        <ContactItemBody name={name} tag={tag}/>
+      <ContactItemImage imageUrl={imageUrl} />
+      <ContactItemBody name={name} tag={tag} />
+      <DeleteButton id={id} onDelete={onDelete} />
     </div>
   )
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 
 import ContactItem from './ContactItem'
 
-const ContactList = ({ contacts }) => {
+
+const ContactList = ({ contacts, onDelete }) => {
   return (
     <div className='contact-list'>
-        {
-            contacts.map((contact) => (
-                <ContactItem key={contact.id} {...contact}/>
-            ))
-        }
+      {
+        contacts.map((contact) => (
+          <ContactItem
+            key={contact.id}
+            id={contact.id}
+            onDelete={onDelete} 
+            
+            {...contact} />
+        ))
+      }
     </div>
   )
 }
